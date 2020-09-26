5c9cdc5b1953efafdca83267cc2639e6719aa797
nadim-khan <khan.nadim58@gmail.com>
2020-09-26 15:55:18 +0530

    updated

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/MyEcommCart'),
+      dir: require('path').join(__dirname, './coverage/gymnasium'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
