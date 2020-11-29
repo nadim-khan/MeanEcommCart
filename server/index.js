65432b111ab113db0d7837dbd1ccda6550094bd0
nadim-khan <khan.nadim58@gmail.com>
2020-11-29 22:36:07 +0530

    port changed

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require('./config/express');
 const config = require('./config/config');
 const mongoose = require('./config/mongoose');
 
-app.listen(config.port||process.env.PORT, (req, res) => {
+app.listen(process.env.PORT||config.port, (req, res) => {
     console.clear();
     console.log("******************************************\n \t   My Cart- MEAN App\n******************************************");
     console.info(`Server started on port ${config.port} (${config.env})`);
