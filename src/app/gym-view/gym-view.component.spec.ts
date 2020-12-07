e0e807b777a8522326eb45a04416e383ec74fd78
nadim-khan <khan.nadim58@gmail.com>
2020-12-06 20:17:42 -0800

    upgraded to angular 11

--- src/app/gym-view/gym-view.component.spec.ts
+++ src/app/gym-view/gym-view.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GymViewComponent } from './gym-view.component';
 
@@ -6,7 +6,7 @@ describe('GymViewComponent', () => {
   let component: GymViewComponent;
   let fixture: ComponentFixture<GymViewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GymViewComponent ]
     })
--- src/app/trainers/trainers.component.spec.ts
+++ src/app/trainers/trainers.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TrainersComponent } from './trainers.component';
 
@@ -6,7 +6,7 @@ describe('TrainersComponent', () => {
   let component: TrainersComponent;
   let fixture: ComponentFixture<TrainersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TrainersComponent ]
     })
